cfd9eb985a3757ac2031cee0a2be36665e00b774
muhammadalief19 <aliefmuhammad1908@gmail.com>
2024-02-22 14:08:13 +0700

    05:UPDATE DATA

--- routes/kategori.js
+++ routes/kategori.js
@@ -42,4 +42,47 @@ router.post("/store", (req, res, next) => {
   }
 });
 
+router.get("/edit/(:id)", (req, res, next) => {
+  let { id } = req.params;
+
+  connection.query(
+    "SELECT * FROM kategori WHERE id_kategori=?",
+    id,
+    (err, rows) => {
+      if (err) {
+        req.flash("error", err);
+      } else {
+        res.render("kategori/update", {
+          data: rows[0],
+        });
+      }
+    }
+  );
+});
+
+router.post("/update/(:id)", (req, res, next) => {
+  try {
+    let { id } = req.params;
+    let { nama_kategori } = req.body;
+    let data = {
+      nama_kategori,
+    };
+    connection.query(
+      "UPDATE kategori set ? where id_kategori = " + id,
+      data,
+      (err, result) => {
+        if (err) {
+          req.flash("error", "Gagal mengupdate data");
+        } else {
+          req.flash("success", "Berhasil mengupdate data");
+        }
+        res.redirect("/kategori");
+      }
+    );
+  } catch (error) {
+    req.flash("error", "Terjadi kesalahan");
+    res.redirect("/kategori");
+  }
+});
+
 module.exports = router;
